6f72d48a3f587c476e6673f583529d6fc6ef072a
Luke Webb <weblu938@student.otago.ac.nz>
2024-01-09 13:41:33 +1300

    Added movement to the EnemyWave via script

--- Assets/Scripts/EnemyWave.cs
+++ Assets/Scripts/EnemyWave.cs
@@ -7,6 +7,12 @@ public class EnemyWave : MonoBehaviour
     // Variable pointing to the object prefab
     public Transform alienPrefab;
 
+    // Speed of the wave movement
+    public float speed;
+
+    // Direction of the wave movement (-ve means left, +ve means right)
+    public int direction = -1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +23,17 @@ public class EnemyWave : MonoBehaviour
             float offsetX = ((y % 2 == 0) ? 0.0f : 0.5f) * gapBetweenAliens;
             for(int x = -3; x < 3; ++x)
             {
-                //Create new game object (from prefab)
+                // Create new game object (from prefab)
                 Transform alien = Instantiate(alienPrefab);
                 alien.parent = transform;
                 // Position the newly create object int he wave
                 alien.localPosition = new Vector3((x * gapBetweenAliens) + offsetX, 0 + (y * gapBetweenAliens), 0);
             }
         }
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(Time.deltaTime * direction * speed, 0, 0));
     }
 }
